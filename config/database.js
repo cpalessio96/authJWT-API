bb8bc5db65900b716d8609ec01566eef430c1ea8
Alessio <alessio.catania@ctmobi.it>
2022-07-09 18:45:33 +0200

    feat(config database): aggiunta funzione per la connessione a mongodb tramite mongoose

---
+++ config/database.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const { MONGO_URI } = process.env;
+
+export const connect = () => {
+  mongoose
+    .connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Successfully connected to database");
+    })
+    .catch((error) => {
+      console.log("database connection failed. exiting now...");
+      console.error(error);
+      process.exit(1);
+    });
+};
