80beec3b71bbf6d88d7fbb5f3b58f998934ec007
Alessio <alessio.catania@ctmobi.it>
2022-07-10 13:03:21 +0200

    feat(change password): aggiunta funzione per il cambio password utente

---
+++ functions/userOperations/changePassword.js
@@ -0,0 +1,50 @@
+import bcrypt from "bcryptjs";
+import User from "../../model/user.js";
+import { checkCredentials } from "../checkCredentials/helper.js";
+
+/**
+ * Verifica che l'utente esiste, verifica la password e crea il token.
+ * Infine torna il code e l'utente
+ * @param {String} param.email
+ * @param {String} param.password
+ * @returns {Object} code e json
+ */
+export const changePassword = async ({ email, newPassword, oldPassword }) => {
+  const sanitizedEmail = email.toLowerCase();
+  // Validate user input
+  if (!sanitizedEmail || !newPassword || !oldPassword) {
+    return {
+      code: 400,
+      json: { message: "all input is required" },
+    };
+  }
+
+  const { success } = await checkCredentials({
+    email: sanitizedEmail,
+    password,
+  });
+
+  if (!success) {
+    return {
+      code: 400,
+      json: { message: "invalid credentials" },
+    };
+  }
+
+  // encrypt password utente
+  const encryptedNewPassword = await bcrypt.hash(newPassword, 10);
+
+  await User.updateOne(
+    { email: sanitizedEmail },
+    { $set: { password: encryptedNewPassword } }
+  );
+
+  return {
+    code: 200,
+    json: {
+      success: true,
+    },
+  };
+};
+
+export default { changePassword };
